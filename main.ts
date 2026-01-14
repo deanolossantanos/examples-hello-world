325ea1200f48a12155d7cc3da69f0b4e28a3896e
zingy11 <37190895+zingy11@users.noreply.github.com>
2026-01-14 23:25:36 +0200

    Refactor serve function to handle async file serving
    Refactor Deno.serve to use async/await and return a Response with headers.

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 import { serveFile } from "jsr:@std/http/file-server";
 
-Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+Deno.serve(async (req: Request) => {
+  const file = await serveFile(req, "./index.html");
+  return new Response(file.body, {
+    status: 200,
+    headers: {
+      "content-type": "text/html; charset=utf-8",
+    },
+  });
 });
