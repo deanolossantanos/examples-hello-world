153c7f7fe376bfe4e91ae7018cb55dab8990aa75
zingy11 <37190895+zingy11@users.noreply.github.com>
2026-01-17 10:20:30 +0200

    Change file serving from serveFile to serveDir

--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
-import { serveFile } from "jsr:@std/http/file-server";
+import { serveDir } from "jsr:@std/http/file-server";
 
-Deno.serve(async (req: Request) => {
-  const file = await serveFile(req, "./index.html");
-  return new Response(file.body, {
-    status: 200,
-    headers: {
-      "content-type": "text/html; charset=utf-8",
-    },
+Deno.serve((req: Request) => {
+  return serveDir(req, {
+    fsRoot: "./dist",
   });
 });
+
